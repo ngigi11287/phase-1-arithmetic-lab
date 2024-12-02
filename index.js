09951cc62848e953a144cd7d97ab522ad38727bb
ngigi11287 <ngigi.11287@gmail.com>
2024-12-02 14:04:37 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+
+let random = Math.floor(Math.random() * 10) + 1;  
+console.log(random);
+
+
+
+let num3 = 24;
+let num4 = 10; 
+let mod = num3 % num4; 
+console.log(mod); 
+
+
+ 
+let set = [5, 12, 20, 7, 18];  
+let max = Math.max(...set);  
+console.log(max);
